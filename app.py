559ea0188c882be131b5fbe87cc147a5afa0edb9
Alifia Salwa S <als.salsa27@gmail.com>
2024-03-29 10:16:05 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from http import client
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.vmgfjkn.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
